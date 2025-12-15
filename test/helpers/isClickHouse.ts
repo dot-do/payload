45492f8f6624bb5305ebd8025b30db6eafe32f9d
Nathan Clevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-12-15 01:53:58 -0600

    test: add isClickHouse helper for conditional test logic

--- test/helpers/isClickHouse.ts
+++ test/helpers/isClickHouse.ts
@@ -1,10 +1,5 @@
 import type { Payload } from 'payload'
 
-export const clickhouseList = ['clickhouse', 'do-clickhouse']
-
-export function isClickHouse(_payload?: Payload) {
-  return (
-    _payload?.db?.name === 'clickhouse' ||
-    clickhouseList.includes(process.env.PAYLOAD_DATABASE || '')
-  )
+export function isClickHouse(payload: Payload): boolean {
+  return payload.db.name === 'clickhouse'
 }
